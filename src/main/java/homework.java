26f1e0d1296ea234cea52add430e94be71d7d4ad
Swapnil Dhanwal <swapnildhanwal@Swapnils-MacBook-Pro-M1-Pro.local>
2023-04-05 11:38:45 -0700

    Add sentence parsing and postfix conversion support

--- src/main/java/homework.java
+++ src/main/java/homework.java
@@ -9,73 +9,115 @@ import java.util.stream.Collectors;
 
 public class homework {
 
-    public static void main(String[] args) {
+    private static final char OPEN_BRACE = '(';
+    private static final char CLOSE_BRACE = ')';
 
+    //each expression is a list of expressions
+    public static void main(String[] args) {
+        SentenceParser parser = new SentenceParser();
+
+        String line = "~A(Akanksha, Potty) & B(Akanksha, Swapnil) | C(Mom, Dad) => D(Swapnil, Family)";
+        Sentence sentence = parser.parse(line.replaceAll("\\s", ""));
+        System.out.println(sentence);
+
+        System.out.println(parser.toPostfix(sentence));
     }
 
-    public static class ExpressionParser {
+    public static class SentenceParser {
 
+        public Sentence parse(String line) {
+            List<Expression> expressions = new ArrayList<>();
+            int i = 0;
+            while (i < line.length()) {
+                Character c = line.charAt(i);
+                if (Operator.isOperator(c)) {
+                    i = parseOperator(line, expressions, i);
+                }
+                else {
+                    i = parsePredicate(line, expressions, i, false);
+                }
+            }
+            return new Sentence(expressions);
+        }
 
+        public Sentence toPostfix(Sentence sentence) {
+            Stack<Operator> stack = new Stack<>();
+            List<Expression> postfix = new ArrayList<>();
+            for (Expression expression : sentence.getExpressions()) {
+                switch (expression.getType()) {
+                    case PREDICATE:
+                        postfix.add(expression);
+                        break;
+                    case OPERATOR:
+                        Operator operator = (Operator) expression;
+                        while (!stack.isEmpty() && operator.getPrecedence() <= stack.peek().getPrecedence()) {
+                            Operator next = stack.pop();
+                            postfix.add(next);
+                        }
+                        stack.add(operator);
+                        break;
+                    default:
+                        throw new IllegalArgumentException(String.format("Expression cannot be of type %s", expression.getType()));
+                }
+            }
+            while (!stack.isEmpty()) {
+                Operator next = stack.pop();
+                postfix.add(next);
+            }
+            return new Sentence(postfix);
+        }
+
+        private int parseOperator(String sentence, List<Expression> expressions, int currentIndex) {
+            Operator operator = Operator.from(sentence.charAt(currentIndex));
+            if (operator == Operator.NOT) {
+                currentIndex += operator.getLength();
+                return parsePredicate(sentence, expressions, currentIndex, true);
+            }
+            expressions.add(operator);
+            return currentIndex + operator.getLength();
+        }
+
+        private int parsePredicate(String sentence, List<Expression> expressions, int currentIndex, boolean negated) {
+            int i;
+            int j;
+            String name;
+            List<String> arguments;
+
+            /* Parse predicate name */
+            i = currentIndex;
+            j = sentence.indexOf(OPEN_BRACE, i);
+            name = sentence.substring(i, j);
+
+            /* Parse argument names */
+            i = j + 1;
+            j = sentence.indexOf(CLOSE_BRACE, i);
+            arguments = Arrays.asList(sentence.substring(i, j).split(","));
+
+            /* Add predicate to the expressions list */
+            Predicate predicate = new Predicate(name, arguments, negated);
+            expressions.add(predicate);
+
+            return j + 1;
+        }
     }
 
     public interface Expression {
 
         ExpressionType getType();
-
-        int getPrecedence();
     }
 
-    public static abstract class UnaryExpression implements Expression {
-
-        protected final Expression expression;
-
-        public UnaryExpression(Expression expression) {
-            this.expression = expression;
-        }
-    }
-
-    public static abstract class BinaryExpression implements Expression {
-
-        protected final Expression first;
-
-        protected final Expression second;
-
-        public BinaryExpression(Expression first, Expression second) {
-            this.first = first;
-            this.second = second;
-        }
-
-        public Expression getFirst() {
-            return first;
-        }
-
-        public Expression getSecond() {
-            return second;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("(%s %s %s)", first.toString(), getType().getOperator(), second.toString());
-        }
-    }
-
-    public static class Predicate extends UnaryExpression {
+    public static class Predicate implements Expression {
 
         private final String name;
 
         private final List<String> arguments;
 
-        public Predicate(String name, List<String> arguments) {
-            super(null);
+        private boolean negated;
+
+        public Predicate(String name, List<String> arguments, boolean negated) {
             this.name = name;
             this.arguments = arguments;
-        }
-
-        public Predicate(String name, String... arguments) {
-            super(null);
-            this.name = name;
-            this.arguments = new ArrayList<>();
-            this.arguments.addAll(Arrays.asList(arguments));
+            this.negated = negated;
         }
 
         @Override
@@ -83,160 +125,131 @@ public class homework {
             return ExpressionType.PREDICATE;
         }
 
-        @Override
-        public int getPrecedence() {
-            return -1;
+        public String getName() {
+            return name;
+        }
+
+        public List<String> getArguments() {
+            return arguments;
+        }
+
+        public boolean isNegated() {
+            return negated;
+        }
+
+        public void setNegated(boolean negated) {
+            this.negated = negated;
         }
 
         @Override
         public String toString() {
-            return String.format("%s(%s)", name, String.join(",", arguments));
+            StringBuilder builder = new StringBuilder();
+            if (isNegated()) {
+                builder.append(Operator.NOT.getPrefix());
+            }
+            builder.append(String.format("%s(%s)", name, String.join(",", arguments)));
+            return builder.toString();
         }
     }
 
-    public static class Negation extends UnaryExpression {
+    public static class Sentence implements Expression {
 
-        public static Negation of(Expression expression) {
-            return new Negation(expression);
-        }
+        private final List<Expression> expressions;
 
-        private Negation(Expression expression) {
-            super(expression);
+        public Sentence(List<Expression> expressions) {
+            this.expressions = expressions;
         }
 
         @Override
         public ExpressionType getType() {
-            return ExpressionType.NEGATION;
+            return ExpressionType.SENTENCE;
         }
 
-        @Override
-        public int getPrecedence() {
-            return 0;
+        public List<Expression> getExpressions() {
+            return expressions;
         }
 
         @Override
         public String toString() {
-            return String.format("~%s", expression.toString());
-        }
-    }
-
-    public static class Conjunction extends BinaryExpression {
-
-        public static Conjunction of(Expression first, Expression second) {
-            return new Conjunction(first, second);
-        }
-
-        private Conjunction(Expression first, Expression second) {
-            super(first, second);
-        }
-
-        @Override
-        public ExpressionType getType() {
-            return ExpressionType.CONJUNCTION;
-        }
-
-        @Override
-        public int getPrecedence() {
-            return 1;
-        }
-    }
-
-    public static class Disjunction extends BinaryExpression {
-
-        public static Disjunction of(Expression first, Expression second) {
-            return new Disjunction(first, second);
-        }
-
-        private Disjunction(Expression first, Expression second) {
-            super(first, second);
-        }
-
-        @Override
-        public ExpressionType getType() {
-            return ExpressionType.DISJUNCTION;
-        }
-
-        @Override
-        public int getPrecedence() {
-            return 2;
-        }
-    }
-
-    public static class Implication extends BinaryExpression {
-
-        public static Implication of(Expression first, Expression second) {
-            return new Implication(first, second);
-        }
-
-        private Implication(Expression first, Expression second) {
-            super(first, second);
-        }
-
-        @Override
-        public ExpressionType getType() {
-            return ExpressionType.IMPLICATION;
-        }
-
-        @Override
-        public int getPrecedence() {
-            return 3;
+            StringBuilder builder = new StringBuilder();
+            for (Expression expression : expressions) {
+                builder.append(expression.toString());
+            }
+            return builder.toString();
         }
     }
 
     public enum ExpressionType {
 
-        PREDICATE(null),
-        NEGATION("~"),
-        CONJUNCTION("&"),
-        DISJUNCTION("|"),
-        IMPLICATION("=>");
-
-        private final String operator;
-
-        private static final Map<String, ExpressionType> labels = Arrays.stream(values())
-                .collect(Collectors.toMap(ExpressionType::getOperator, Function.identity()));
-
-        public static ExpressionType from(String label) {
-            if (!labels.containsKey(label)) {
-                throw new IllegalArgumentException(String.format("Label %s must belong to one of %s", label, Arrays.toString(values())));
-            }
-            return labels.get(label);
-        }
-
-        ExpressionType(String operator) {
-            this.operator = operator;
-        }
-
-        public String getOperator() {
-            return operator;
-        }
+        PREDICATE,
+        OPERATOR,
+        SENTENCE;
     }
 
-    public enum OperatorType {
+    public enum Operator implements Expression {
 
-        NOT("~"),
-        AND("&"),
-        OR("|"),
-        IMPLIES("=>");
+        NOT('~', "~", 1, 3),
+        AND('&', "&", 1, 2),
+        OR('|', "|", 1, 1),
+        IMPLIES('=', "=>", 2, 0);
 
-        private static final Map<String, OperatorType> labelsMap = Arrays.stream(values())
-                .collect(Collectors.toMap(OperatorType::getLabel, Function.identity()));
+        private static final Map<Character, Operator> prefixMap = Arrays.stream(values())
+                .collect(Collectors.toMap(Operator::getPrefix, Function.identity()));
+
+        private static final Set<Character> prefixes = Arrays.stream(values())
+                .map(Operator::getPrefix)
+                .collect(Collectors.toSet());
+
+        private final char prefix;
 
         private final String label;
 
-        public static OperatorType from(String label) {
-            if (!labelsMap.containsKey(label)) {
-                throw new IllegalArgumentException(String.format("Label must be one of %s", Arrays.toString(values())));
+        private final int length;
+
+        private final int precedence;
+
+        public static Operator from(Character prefix) {
+            if (!prefixMap.containsKey(prefix)) {
+                throw new IllegalArgumentException(String.format("Prefix must be one of %s", Arrays.toString(values())));
             }
-            return labelsMap.get(label);
+            return prefixMap.get(prefix);
         }
 
-        OperatorType(String label) {
+        Operator(char prefix, String label, int length, int precedence) {
+            this.prefix = prefix;
             this.label = label;
+            this.length = length;
+            this.precedence = precedence;
+        }
+
+        public static boolean isOperator(Character c) {
+            return prefixes.contains(c);
+        }
+
+        @Override
+        public ExpressionType getType() {
+            return ExpressionType.OPERATOR;
+        }
+
+        public char getPrefix() {
+            return prefix;
         }
 
         public String getLabel() {
             return label;
+        }
+
+        public int getLength() {
+            return length;
+        }
+
+        public int getPrecedence() {
+            return precedence;
+        }
+
+        @Override
+        public String toString() {
+            return getLabel();
         }
     }
 }
